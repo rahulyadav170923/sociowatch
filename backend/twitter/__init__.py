029820b7e3505cd4487481d62d58ae1a93a23213
rahu yadav <rahulyadav170923@gmail.com>
2016-08-12 01:14:31 +0530

    added enviroment variables

--- backend/twitter/__init__.py
+++ backend/twitter/__init__.py
@@ -1,11 +1,12 @@
-from backend.twitter.twitter import historical_tweets,twitter_key
+from backend.twitter.twitter import historical_tweets
 from pymongo import MongoClient
 import tweepy
+import os
 
 
 Client=MongoClient()
 db=Client['sociowatch']
 # initialise api instance
-auth = tweepy.OAuthHandler(twitter_key['consumerKey'], twitter_key['consumerSecret'])
-auth.set_access_token(twitter_key['accessToken'], twitter_key['accessTokenSecret'])
+auth = tweepy.OAuthHandler(os.environ['consumerKey'], os.environ['consumerSecret'])
+auth.set_access_token(os.environ['accessToken'], os.environ['accessTokenSecret'])
 api = tweepy.API(auth)
---
+++ keys.py
@@ -0,0 +1,6 @@
+twitter_key={
+  "consumerKey": "eQAWZELmYLo0jIDoj4WxVOFOL",
+  "consumerSecret": "sHLC19ktgqBIktpDwnBKh0wutakAySnEajrdWidbwcoXZ9lrzc",
+  "accessToken": "3130630774-kiiDGsJiSgeFQVKcQZvmWxIz4NoXfoiBut3bMrj",
+  "accessTokenSecret": "za6EOgU2ql0R29v6YE9jNgvmfIv0OaWVkY2OPlFz3pUiU"
+}
